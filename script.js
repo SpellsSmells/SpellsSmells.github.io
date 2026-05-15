ba47d2858dfa2fa95dc4af0815fae444f4cc0b79
SpellsSmells <hardcoretibiagamer@gmail.com>
2026-05-15 03:40:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ async function refreshData() {
             <li class="list-item ${p.area_id}" 
                 onmouseover="syncHighlight('${p.area_id}')" 
                 onmouseout="syncUnhighlight('${p.area_id}')">
-                <strong>${p.name}</strong> Lvl ${p.lvl} ${p.voc}
+                <strong>${p.name}</strong> ${p.lvl} ${p.voc}
             </li>`).join('');
 
         // 2. Render Heatmap
